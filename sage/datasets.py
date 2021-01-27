ba79e8815f794709892c09dd2a09bb92e86e005c
Seneha <68332590+senehasubramaniyan@users.noreply.github.com>
2021-01-27 22:43:21 +0530

    Update datasets.py

--- sage/datasets.py
+++ sage/datasets.py
@@ -38,7 +38,7 @@ def bank():
     df['Success'] = (df['Success'] == 'yes')
     return df
 
-def malware():
+def Malware():
     columns = ['c1','c2','c3','c4','c5','c6','c7','c8','c9','c10','c11','c12','c13','c14','c15','c16']
 
     path = os.path.join(github_data_url, 'Malware.csv')
